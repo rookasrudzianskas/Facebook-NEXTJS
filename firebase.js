f0ab94e1a8bafc04bb48a36f611127d233ee368b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-28 08:07:23 +0300

    Rokas

--- firebase.js
+++ firebase.js
@@ -14,3 +14,7 @@ const firebaseConfig = {
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
+const db = app.firestore();
+const storage = firebase.storage();
+
+export {db, storage};
