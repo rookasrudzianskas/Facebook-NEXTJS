e91d0a5ce2f1c0220803b693e13887e73fe17d91
Rokas <rookas.rudzenskas@gmail.com>
2021-05-28 09:36:11 +0300

    Rokas

--- components/Post.js
+++ components/Post.js
@@ -5,8 +5,56 @@ import Image from "next/image";
 
 const Post = ({ name, message, email, postImage, image, timestamp }) => {
     return (
-        <div>
-            
+        <div className="flex flex-col">
+            <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+                <div className="flex items-center space-x-2">
+                    <img className="rounded-full" src={image ? image :
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-7.536 5.879a1 1 0 001.415 0 3 3 0 014.242 0 1 1 0 001.415-1.415 5 5 0 00-7.072 0 1 1 0 000 1.415z" clipRule="evenodd" />
+                        </svg>}
+                         width={40} height={40} alt=""/>
+
+                    <div>
+                        <p className="font-medium">{name ? name : "Guest 🙂"}</p>
+
+                        <p className="text-xs text-gray-400">
+                            {timestamp ? new Date(timestamp?.toDate()).toLocaleDateString() : "The date is loading..." }
+                        </p>
+                    </div>
+                </div>
+
+                <p className="pt-4">{message}</p>
+            </div>
+
+            {postImage && (
+                <div className="relative h-56 md:h-96 bg-white">
+                    <Image
+                        src={postImage}
+                        objectFit="cover"
+                        layout="fill"
+                    />
+                </div>
+
+            )}
+
+        {/*    footer   */}
+
+            <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+                <div className="inputIcon rounded-none rounded-bl-2xl">
+                    <ThumbUpIcon className="h-4" />
+                    <p className="text-xs sm:text-base">Like</p>
+                </div>
+
+                <div className="inputIcon rounded-none">
+                    <ChatAltIcon className="h-4" />
+                    <p className="text-xs sm:text-base">Comment</p>
+                </div>
+
+                <div className="inputIcon rounded-none rounded-br-2xl">
+                    <ShareIcon className="h-4" />
+                    <p className="text-xs sm:text-base">Share</p>
+                </div>
+            </div>
         </div>
     );
 };
