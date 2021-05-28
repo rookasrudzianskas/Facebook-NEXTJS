e5a2ee11a8002e76dcb3c216232c06ff27960c90
Rokas <rookas.rudzenskas@gmail.com>
2021-05-28 08:01:22 +0300

    Rokas

--- components/InputBox.js
+++ components/InputBox.js
@@ -31,18 +31,18 @@ const InputBox = () => {
             </form>
         </div>
 
-            <div>
-                <div>
+            <div className="flex justify-evenly p-3 border-t">
+                <div className="inputIcon">
                     <VideoCameraIcon className="h-7 text-red-500" />
                     <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
                 </div>
 
-                <div>
+                <div className="inputIcon">
                     <CameraIcon className="h-7 text-green-400" />
                     <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
                 </div>
 
-                <div>
+                <div className="inputIcon">
                     <EmojiHappyIcon className="h-7 text-yellow-300" />
                     <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
                 </div>
