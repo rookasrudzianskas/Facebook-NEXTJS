2e6d480f7db202604a8240953459ca4c49aaf82d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-28 07:43:27 +0300

    Rokas

--- components/Feed.js
+++ components/Feed.js
@@ -3,8 +3,8 @@ import Stories from "./Stories";
 
 const Feed = () => {
     return (
-        <div className="flex-grow h-screen pb-44 pt-6">
-            <div>
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+            <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
             {/*    Stories*/}
                 <Stories />
             {/*    inputbox*/}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -19,18 +19,19 @@ const Sidebar = () => {
 
     return (
         <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
-            <div>
+            <div className="rounded-full flex items-center space-x-2 p-4 hover:bg-gray-200 rounded-xl cursor-pointer">
                 {session ? (
                     <Image
                         src={session.user.image}
                         width={30}
                         height={30}
-                        className="rounded-full"
+                        className="rounded-full flex items-center space-x-2 p-4 hover:bg-gray-200 rounded-xl cursor-pointer"
                         layout="fixed" />
                 ) : (
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+
                               d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                     </svg>
                 )}
